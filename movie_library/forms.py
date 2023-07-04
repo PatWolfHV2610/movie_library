59fcffc73778f1510cc34f8e1471225828814fd3
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-07-04 22:59:39 +0700

    create custome form

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField, URLField
 from wtforms.validators import InputRequired, NumberRange
 
 
 class MovieForm(FlaskForm):
-
     title = StringField("Title", validators=[InputRequired()])
     director = StringField("Director", validators=[InputRequired()])
     year = IntegerField(
@@ -15,3 +14,25 @@ class MovieForm(FlaskForm):
     submit = SubmitField("Add movie")
 
 
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendedMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringListField("Series")
+    tags = StringListField("Tags")
+    description = TextAreaField("Description")
+    video_link = URLField("Video Link")
+
+    submit = SubmitField("Submit")
